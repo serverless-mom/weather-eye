5522488bba9b025131f36cfec90f4250b3719bf7
Toby Fee <TobyFee@gmail.com>
2018-05-02 17:12:48 -0700

    npm updates, switched to the right port, prep for Heroku deploy

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express();
 app.use(express.static('src'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/seven', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, () => console.log(
+  'Weather-eye is listening on port 3000!'));
